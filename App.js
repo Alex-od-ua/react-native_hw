d5d97e03b6252b352000e5762ad917e3d17227ff
Alex <ais_alex@ukr.net>
2023-05-31 16:36:33 +0300

    fix

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import * as Location from "expo-location";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { TouchableWithoutFeedback, Keyboard } from "react-native";
 
-// import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/Screens/mainScreens/Navigation";
 
-// import AuthNavigation from "./src/Screens/auth/AuthNav";
-import Navigation from "./src/Navigation/Navigation";
-// import LoginScreen from "./src/Screens/auth/LoginScreen";
-// import RegistrationScreen from "./src/Screens/auth/RegistrationScreen";
-
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -20,18 +28,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-// alignItems: "center",
-// justifyContent: "center",
-// backgroundColor: "#ecf0f1",
-// },
-
-// image: {
-//   flex: 1,
-//   resizeMode: "cover",
-//   justifyContent: "center",
-// },
-// });
